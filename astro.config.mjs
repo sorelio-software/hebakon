6c7d8730306060a77ad46449a02d6887e5f99605
Kristián Řehoř <kmancz.kiki@gmail.com>
2023-08-19 16:41:06 +0200

    No more gh pages :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-	site: "https://Legit-visual.github.io",
-	base: "/Hebakon",
-	output: "static",
-});
+export default defineConfig({});
